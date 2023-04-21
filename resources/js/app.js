008b599141bdd921b95023634413800340afe8f8
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2023-04-21 17:21:22 +0300

    change font, design nav bar to be presentable, return routing configurations back to app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,37 @@
 import Vue from "vue";
-import router from "./router";
+import axios from 'axios';
+import VueRouter from "vue-router";
+import ExampleComponent from '../components/ExampleComponent.vue';
+import UserList from '../components/UserList.vue';
+import UserForm from "../components/UserForm.vue";
+import StatusList from "../components/StatusList.vue";
+import StatusForm from "../components/StatusForm.vue";
+import TaskList from "../components/TaskList.vue";
+import TaskForm from "../components/TaskForm.vue";
 
 Vue.component('example-component', ExampleComponent);
 Vue.component('user-list', UserList);
 
+Vue.use(VueRouter); 
+
+// 👇 route configuration //
+const routes = [
+    { path: "/", component: UserList },
+    { path: "/user-form", component: UserForm },
+    { path: "/status-list", component: StatusList },
+    { path: "/status-form", component: StatusForm },
+    { path: "/task-list", component: TaskList },
+    { path: "/task-form", component: TaskForm },
+];
+
+const router = new VueRouter({
+    mode: "history",
+    routes,
+});
 
 const app = new Vue({
     el: '#app',
     router, // 👈 add the router instance to the vue instance //
 });
 
+export default router;
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
 
         <title>WorkFlow Hub</title>
 
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
@@ -13,9 +15,38 @@
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     </head>
     <body class="antialiased">
+        
         <div id="app">
-            <example-component></example-component>
-            <user-list></user-list>
+            <!-- Add navigation links -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">WorkFlow Hub</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/">User List</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/user-form">User Form</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/status-list">Status List</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/status-form">Status Form</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/task-list">Task List</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/task-form">Task Form</router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+            <router-view></router-view>
         </div>
 
         <!-- Scripts -->
