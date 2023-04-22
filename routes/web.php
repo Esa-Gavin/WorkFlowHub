d1300fda5da421e2d2c1b7c0c5dd497994f217ea
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2023-04-22 05:39:20 +0300

    temporary change to test

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'root path is working';
+    return view('welcome');
 });
 
 Route::get('/test', function () {
